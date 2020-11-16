153acb29deb5efcdeb213fc207719535d35ad833
mccamatt92 <matthias.mccarthy@gmail.com>
2020-11-16 22:49:04 +0100

    Completed First interation of jumbotron

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React from 'react'
+import jumboData from './fixtures/jumbo';
+import Jumbotron from './components/jumbotron/index'
+
 
 
 export default function App() {
   return (
-    <div className="App">
-      <p>Hello</p>
-      </div>
+  <Jumbotron.Container>
+       {jumboData.map((item) => (
+          <Jumbotron key={item.id} direction={item.direction}>
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{item.title}</Jumbotron.Title>
+              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+         </Jumbotron.Pane>
+          <Jumbotron.Pane>
+             <Jumbotron.Image src={item.image} alt={item.alt}/>
+          </Jumbotron.Pane>
+        
+        </Jumbotron>
+
+     ))}
+   </Jumbotron.Container>
   );
 }
 
