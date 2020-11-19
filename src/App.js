81b15ba26354983216ac19cc58fc7ce9582fa7a0
mccamatt92 <matthias.mccarthy@gmail.com>
2020-11-19 18:51:54 +0100

    Completed Footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { JumbotronContainer } from './containers/jumbotron'
-
+import {FooterContainer} from './containers/footer'
 
 
 
 
 export default function App() {
   return (
+    <>
     <JumbotronContainer/>
+    <FooterContainer/>
+    </>
   )
 }
 
