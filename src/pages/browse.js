b01bb259add9cedcde1560538a20164cba7ef354
mccamatt92 <matthias.mccarthy@gmail.com>
2020-12-06 17:27:29 +0100

    Working Browse getting data from firebase

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import{useContent} from '../hooks/'
+
 // import { BrowseContainer } from '../containers/browse';
 // import { useContent } from '../hooks';
 // import { selectionFilter } from '../utils';
 
 export default function Browse() {
+    const{series} =useContent('series')
+    const{films} =useContent('films')
+    console.log(series)
+    console.log(films)
+
             return<p>Hello from the sign in page</p>
-
-//   const { series } = useContent('series');
-//   const { films } = useContent('films');
-//   const slides = selectionFilter({ series, films });
-
-//   return <BrowseContainer slides={slides} />;
 }
