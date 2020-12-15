1f44cc8105e3ca9dadfa106240e7569a44a8d084
mccamatt92 <matthias.mccarthy@gmail.com>
2020-12-15 22:08:28 +0100

    Add loading omponents,

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
+import Fuse from 'fuse.js';
+import { Card, Header, Loading, Player } from '../components';
 import * as ROUTES from '../constants/routes';
 import logo from '../logo.svg';
 import { FirebaseContext } from '../context/firebase';
@@ -6,8 +8,26 @@ import { SelectProfileContainer } from './profiles';
 import { FooterContainer } from './footer';
 
 export function BrowseContainer({ slides }) {
+  const [profile,setProfile] = useState({});
+  const[loading,setLoading] = useState(true)
   const { firebase } = useContext(FirebaseContext);
   const user = firebase.auth().currentUser || {};
 
-return <SelectProfileContainer user={user}/>
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setLoading(false)
+    },3000)
+  },[profile.displayName]);
+
+return profile.displayName ?(
+    <>
+      {loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+      <Header src="">
+      </Header>
+    </>
+):
+(
+ <SelectProfileContainer user={user} setProfile={setProfile}/>
+);
 }
